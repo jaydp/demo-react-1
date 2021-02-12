47e0a768cbb8228c8c22a9b4ec8fcb4929dd3579
Jaydeep <jaydeepp@fusion-solutions.com.au>
2021-02-12 14:39:59 +1030

    setup components

---
+++ src/index.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+import './index.css';
+
+function formatDate(date) {
+    return date.toLocaleDateString();
+}
+
+function Avatar(props) {
+    return(
+        <img 
+            className="Avatar"
+            src={props.user.avatarUrl}
+            title={props.user.name}
+        />
+    );
+}
+
+function UserInfo(props) {
+    return (
+        <div className="UserInfo">
+            <Avatar user={props.user} />
+            <div className="UserInfo-name">
+                {props.user.name}
+            </div>
+        </div>
+    );
+}
+
+function Comment(props){
+    return (
+        <div className="Comment">
+            <UserInfo user={props.author} />
+            <div className="Comment-text">
+                {props.text}
+            </div>
+            <div className="Comment-date">
+                {formatDate(props.date)}
+            </div>
+        </div>
+    );
+}
+
+const comment = {
+    date: new Date(),
+    text: 'Test 1',
+    author: {
+        name: 'Jaydeep',
+        avatarUrl: 'https://placekitten.com/g/64/64'
+    },
+};
+
+ReactDOM.render(
+    <Comment
+        date={comment.date}
+        text={comment.text}
+        author={comment.author}
+    />,
+    document.getElementById('root')
+)
+
+
+//serviceWorker.unregister();
